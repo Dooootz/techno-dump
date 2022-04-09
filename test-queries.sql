cebf181f1a9661325acea7659b7e3b58da105ae4
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-04-09 15:31:45 +1000

    reference template

---
+++ test-queries.sql
@@ -0,0 +1,12 @@
+-- Query 1:   	List all employers that have more than 2 positions available for software developers
+
+-- Query 2: 	List all candidates that have more than 5 years’ experience in software testing
+
+-- Query 3: 	List all the positions available for software developers including the qualifications and years of experience required.
+
+-- Query 4:  	Display the ‘best fit’ list of candidate for a specific database analyst position
+
+-- Query 5: 	Display the visa status for all web designers. Include the candidate name, surname and phone number. 
+
+-- Query 6: 	List the position title, description, responsibilities, skills requirements, and years of experience, salary range,
+            --  location, and contract type for all positions where the salary is over $150 000. Group the results by position type.
